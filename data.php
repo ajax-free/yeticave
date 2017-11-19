1c3c162db7a22f562bd7bd445386220dccaded6b
Andrey Efimov <efiand@ya.ru>
2017-11-20 01:08:10 +0300

    Лоты

--- data.php
+++ data.php
@@ -1,13 +1,9 @@
 <?php
-// список категорий
-$categories_list = [
-    'boards' => 'Доски и лыжи',
-    'attachment' => 'Крепления',
-    'boots' => 'Ботинки',
-    'clothing' => 'Одежда',
-    'tools' => 'Инструменты',
-    'other' => 'Разное'
-];
+// устанавливаем часовой пояс в Московское время
+date_default_timezone_set('Europe/Moscow');
+
+// подключаем данные лотов
+require 'data_lots.php';
 
 // общие данные сайта
 $layout_data = [
@@ -15,49 +11,13 @@ $layout_data = [
     'user_name' => 'Константин',
     'user_avatar' => 'img/user.jpg',
     'categories_list' => $categories_list,
-    'is_auth' => (bool) rand(0, 1)
+    'is_auth' => (bool) rand(0, 1),
+    'index_link' => 'href="/" '
 ];
 
 // данные главной страницы
 $index_data = [
     'categories_list' => $categories_list,
-    // список объявлений
-    'announcements_list' => [
-        [
-            'name' => '2014 Rossignol District Snowboard',
-            'category' => 'boards',
-            'price' => 10999,
-            'picture' => 'img/lot-1.jpg'
-        ],
-        [
-            'name' => 'DC Ply Mens 2016/2017 Snowboard',
-            'category' => 'boards',
-            'price' => 159999,
-            'picture' => 'img/lot-2.jpg'
-        ],
-        [
-            'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-            'category' => 'attachment',
-            'price' => 8000,
-            'picture' => 'img/lot-3.jpg'
-        ],
-        [
-            'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-            'category' => 'boots',
-            'price' => 10999,
-            'picture' => 'img/lot-4.jpg'
-        ],
-        [
-            'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-            'category' => 'clothing',
-            'price' => 7500,
-            'picture' => 'img/lot-5.jpg'
-        ],
-        [
-            'name' => 'Маска Oakley Canopy',
-            'category' => 'other',
-            'price' => 5400,
-            'picture' => 'img/lot-6.jpg'
-        ]
-    ]
+    // список объявлений (вынесли в отдельный файл в module4-task1)
+    'announcements_list' => $lots_list
 ];
